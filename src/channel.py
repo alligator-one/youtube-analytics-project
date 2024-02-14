8942cabdaac42e9466313af3aa7dfae12a93bfc4
cniimf_ot <ot@cniimf.ru>
2024-02-14 19:52:56 +0300

    add channel class

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,32 @@
+import json
+import os
+from googleapiclient.discovery import build
+
+
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key = 'AIzaSyD18XmtcKqilmDbM-tOqvnGAUhH8SEiFXs'
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         pass
 
-    def print_info(self) -> None:
+    def print_info(self, channel_id)  -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+
+        self.channel_id = channel_id
+        api_key = 'AIzaSyD18XmtcKqilmDbM-tOqvnGAUhH8SEiFXs'
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel_id = 'UCwHL6WHUarjGfUM_586me8w'
+        self.channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+
+    def print_info(self):
+        """Выводит в консоль информацию о канале."""
+        # pass
+
+        printj(self.channel)
